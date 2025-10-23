ab10806bbcc7a3a7ebc6ddcda4d890ff88cf93c8
wayne <wayne.wan-chow-wah@etu.umontpellier.fr>
2025-10-24 01:40:14 +0400

    Add gateway microservice

---
+++ back/store-gateway/src/test/java/com/shopifaille/store_gateway/StoreGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopifaille.store_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
