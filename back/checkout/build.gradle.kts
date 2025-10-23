e23827091533db4a7dcefcc069b1c2b5c6627094
wayne <wayne.wan-chow-wah@etu.umontpellier.fr>
2025-10-24 01:47:53 +0400

    Add dependencies

--- back/checkout/build.gradle.kts
+++ back/checkout/build.gradle.kts
@@ -20,16 +20,68 @@ repositories {
 	mavenCentral()
 }
 
+val log4jBomVersion = "2.24.3"
+val grpcVersion = "1.73.0"
+val grpcKotlinVersion = "1.4.3"
+val protobufVersion = "4.31.1"
+val coroutinesVersion = "1.10.2"
+val arrowKtVersion = "2.1.2"
+val kotestVersion = "5.9.1"
+val kotestArrowVersion = "2.0.0"
+val kotlinLoggingJvmVersion = "7.0.3"
+
 dependencies {
+	implementation(platform("io.arrow-kt:arrow-stack:$arrowKtVersion"))
+    implementation("io.arrow-kt:arrow-core")
+    implementation("io.arrow-kt:arrow-fx-coroutines")
+    implementation("io.arrow-kt:arrow-autoclose")
+    implementation("io.arrow-kt:suspendapp")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation(platform("org.apache.logging.log4j:log4j-bom:$log4jBomVersion"))
+    implementation("org.apache.logging.log4j:log4j-slf4j2-impl")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:$coroutinesVersion")
+    implementation("io.grpc:grpc-protobuf:$grpcVersion")
+    implementation("io.grpc:grpc-stub:$grpcVersion")
+    implementation("io.grpc:grpc-kotlin-stub:$grpcKotlinVersion")
+    implementation("io.grpc:grpc-netty-shaded:$grpcVersion")
+    implementation("com.google.protobuf:protobuf-kotlin:$protobufVersion")
+	implementation("io.github.oshai:kotlin-logging-jvm:$kotlinLoggingJvmVersion")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation(kotlin("test"))
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-property:$kotestVersion")
+    testImplementation("io.kotest.extensions:kotest-assertions-arrow:$kotestArrowVersion")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:$protobufVersion"
+    }
+    plugins {
+        create("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
+        }
+        create("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:$grpcKotlinVersion:jdk8@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                create("grpc")
+                create("grpckt")
+            }
+        }
+    }
 }
 
 kotlin {
