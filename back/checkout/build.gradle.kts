9ff978731d327dfebc9a8c3082a754b5aa532138
wayne <wayne.wan-chow-wah@etu.umontpellier.fr>
2025-10-24 01:52:51 +0400

    Fix protobuf plugin

--- back/checkout/build.gradle.kts
+++ back/checkout/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 	kotlin("jvm") version "1.9.25"
 	kotlin("plugin.spring") version "1.9.25"
+	id("com.google.protobuf") version "0.9.5"
 	id("org.springframework.boot") version "3.5.6"
 	id("io.spring.dependency-management") version "1.1.7"
 	kotlin("plugin.jpa") version "1.9.25"
